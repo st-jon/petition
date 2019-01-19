d198b5f37b19e571e36a9e54e1df59d02e32cda3
christophe <c.johanny@gmail.com>
2019-01-19 23:30:49 +0100

    some little update

--- public/slideshow.js
+++ public/slideshow.js
@@ -1,25 +1,28 @@
-$(function () {
+(function () {
     
-    var change_img_time = 5000	
-    var transition_speed = 300  
-    var simple_slideshow = $(".selfie__container")
-    var listItems = simple_slideshow.children('li')
+    var delay = 5000	
+    var transition = 300  
+    var slideshow = $(".selfie__container")
+    var listItems = slideshow.children('li')
     var listLen	= listItems.length
     var i = 0
 		
     changeList = function () {
-    
-        listItems.eq(i).fadeOut(transition_speed, function () {
-            i += 1;
+        listItems.eq(i).fadeOut(transition, function () {
+            i += 1
             if (i === listLen) {
-                i = 0;
+                i = 0
             }
-            listItems.eq(i).fadeIn(transition_speed);
-        });
-
-    };
+            listItems.eq(i).fadeIn(transition)
+        })
+    }
 		
-    listItems.not(':first').hide();
-    setInterval(changeList, change_img_time);
+    listItems.not(':first').hide()
+    setInterval(changeList, delay)
+
+    $('li').on('click', function() {
+        listItems.not(':first').hide()
+        changeList()
+    })
 	
-})
+})()
